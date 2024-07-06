5aa32a8d95b2e9bdc7f303ec16c4fe331dee876e
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-07-06 15:34:12 +0700

    feat(voucherServiceImpl) add: addvoucher

--- src/main/java/com/mini_project_event_management/event_management/voucher/repository/VoucherRepository.java
+++ src/main/java/com/mini_project_event_management/event_management/voucher/repository/VoucherRepository.java
@@ -4,4 +4,6 @@ import com.mini_project_event_management.event_management.voucher.entity.Voucher
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoucherRepository extends JpaRepository<Voucher, Long> {
+    Boolean existByName(String name);
+    Boolean existByCode(String code);
 }
--- src/main/java/com/mini_project_event_management/event_management/voucher/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/mini_project_event_management/event_management/voucher/service/impl/VoucherServiceImpl.java
@@ -1,8 +1,10 @@
 package com.mini_project_event_management.event_management.voucher.service.impl;
 
+import com.mini_project_event_management.event_management.exceptions.AlreadyExistException;
 import com.mini_project_event_management.event_management.organizer.entity.Organizer;
 import com.mini_project_event_management.event_management.organizer.service.OrganizerService;
 import com.mini_project_event_management.event_management.voucher.dto.VoucherDto;
+import com.mini_project_event_management.event_management.voucher.entity.Voucher;
 import com.mini_project_event_management.event_management.voucher.repository.VoucherRepository;
 import com.mini_project_event_management.event_management.voucher.service.VoucherService;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,25 @@ public class VoucherServiceImpl implements VoucherService {
 
      @Override
     public void addVoucher(VoucherDto voucherDto){
-         Organizer organizer = organizerService.
+         Organizer organizer = organizerService.getOrganizerById(voucherDto.getOrganizerId());
+         Voucher voucher = new Voucher();
+
+         Boolean isNameExist = voucherRepository.existByName(voucherDto.getName());
+         if (isNameExist){
+             throw new AlreadyExistException("Name already exist");
+         }
+
+         Boolean isCodeExist = voucherRepository.existByCode(voucherDto.getCode());
+         if(isCodeExist){
+             throw new AlreadyExistException("Code already exist");
+         }
+
+         voucher.setCode(voucherDto.getCode());
+         voucher.setName(voucherDto.getName();
+         voucher.setOrganizer(organizer);
+         voucher.setDiscountPercent(voucherDto.getDiscountPercent());
+
+         voucherRepository.save(voucher);
      }
+
 }
