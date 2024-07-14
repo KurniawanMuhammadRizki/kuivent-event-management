fb1125d39a14357656f508adaa5cf756c5c00e72
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-07-14 20:32:19 +0700

    feat(block) add: add getBlockById

--- src/main/java/com/mini_project_event_management/event_management/block/service/impl/BlockServiceImpl.java
+++ src/main/java/com/mini_project_event_management/event_management/block/service/impl/BlockServiceImpl.java
@@ -6,7 +6,11 @@ import com.mini_project_event_management.event_management.block.repository.Block
 import com.mini_project_event_management.event_management.block.service.BlockService;
 import com.mini_project_event_management.event_management.category.entity.Category;
 import com.mini_project_event_management.event_management.category.service.CategoryService;
+import com.mini_project_event_management.event_management.exceptions.DataNotFoundException;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BlockServiceImpl implements BlockService {
@@ -23,5 +27,15 @@ public class BlockServiceImpl implements BlockService {
           Block block = blockDto.toEntity();
           block.setCategory(category);
           blockRepository.save(block);
+     }
+
+     @Override
+     @Cacheable(value = "getBlockById", key = "#id")
+     public Block getBlockById(Long id){
+          Optional<Block> block = blockRepository.findById(id);
+          if(block.isEmpty() || block == null){
+               throw new DataNotFoundException("Block not found");
+          }
+          return block.orElse(null);
      }
 }
