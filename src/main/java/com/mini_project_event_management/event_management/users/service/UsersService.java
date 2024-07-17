57508f960a113e2b5dfecfcaed29b8bac8e0ee07
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-07-18 00:44:32 +0700

    feat(users) add: add getAuthorizedUser and reformat code

--- src/main/java/com/mini_project_event_management/event_management/users/service/UsersService.java
+++ src/main/java/com/mini_project_event_management/event_management/users/service/UsersService.java
@@ -2,6 +2,7 @@ package com.mini_project_event_management.event_management.users.service;
 
 import com.mini_project_event_management.event_management.users.dto.RegisterUserRequestDto;
 import com.mini_project_event_management.event_management.users.dto.RegisterUserResponseDto;
+import com.mini_project_event_management.event_management.users.dto.UsersDto;
 import com.mini_project_event_management.event_management.users.entity.Users;
 import lombok.Data;
 
@@ -12,4 +13,6 @@ public interface UsersService {
      Users getUserById(Long id);
 
      Users getUserByEmail(String email);
+
+     UsersDto getUsers();
 }
--- src/main/java/com/mini_project_event_management/event_management/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/mini_project_event_management/event_management/users/service/impl/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.mini_project_event_management.event_management.users.service.impl;
 
 import com.mini_project_event_management.event_management.exceptions.DataNotFoundException;
+import com.mini_project_event_management.event_management.helpers.CurrentUser;
 import com.mini_project_event_management.event_management.helpers.SlugifyHelper;
 import com.mini_project_event_management.event_management.users.dto.RegisterUserRequestDto;
 import com.mini_project_event_management.event_management.users.dto.RegisterUserResponseDto;
+import com.mini_project_event_management.event_management.users.dto.UsersDto;
 import com.mini_project_event_management.event_management.users.entity.Users;
 import com.mini_project_event_management.event_management.users.repository.UsersRepository;
 import com.mini_project_event_management.event_management.users.service.UsersService;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,9 +23,12 @@ public class UserServiceImpl implements UsersService {
      private final UsersRepository usersRepository;
      private final PasswordEncoder passwordEncoder;
 
-     public UserServiceImpl(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
+     private final CurrentUser currentUser;
+
+     public UserServiceImpl(UsersRepository usersRepository, PasswordEncoder passwordEncoder, @Lazy CurrentUser currentUser) {
           this.usersRepository = usersRepository;
           this.passwordEncoder = passwordEncoder;
+          this.currentUser = currentUser;
      }
 
      @Override
@@ -64,6 +70,13 @@ public class UserServiceImpl implements UsersService {
                throw new DataNotFoundException("User not found");
           }
           return user.orElse(null);
+     }
+
+     @Override
+     public UsersDto getUsers() {
+          Long userId = currentUser.getAuthorizedUsersId();
+          Users users = getUserById(userId);
+          return users.toUsersDto();
      }
 
 }
