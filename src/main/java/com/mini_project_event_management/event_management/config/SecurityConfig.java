b273cd0dff85d055239ba7d0952f4e3bbbfb21ab
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-07-17 22:35:26 +0700

    feat(auth) add: protect voucher endpoint

--- src/main/java/com/mini_project_event_management/event_management/config/SecurityConfig.java
+++ src/main/java/com/mini_project_event_management/event_management/config/SecurityConfig.java
@@ -76,6 +76,7 @@ public class SecurityConfig {
                      auth.requestMatchers("api/v1/event-topic/**").permitAll();
                     auth.requestMatchers("api/v1/user/forget-password").permitAll();
                     auth.requestMatchers(HttpMethod.POST,"api/v1/voucher").hasAuthority("SCOPE_ROLE_ORGANIZER");
+                     auth.requestMatchers("api/v1/voucher/organizer").hasAuthority("SCOPE_ROLE_ORGANIZER");
                     auth.anyRequest().authenticated();
                 }).sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt((jwt) -> jwt.decoder(jwtDecoder()))).userDetailsService(userDetailsService).httpBasic(Customizer.withDefaults()).build();
